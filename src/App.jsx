c17deeba908a82a22014323ac03f7878aacf43b3
Mohamud E Hasen <mohmudes4@gmail.com>
2023-07-17 18:55:05 -0500

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TopBar from './conponents/TopBar'
 import Navbar from './conponents/Navbar'
+import Heromg from './conponents/Herosec'
 
 const App = () => {
   return (
     <div>
       <TopBar />
       <Navbar />
+      <Heromg />
     </div>
   )
 }
